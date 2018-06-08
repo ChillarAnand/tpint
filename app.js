8b4e0152be83619372ef11b54fb0e06ccd071013
Chillar Anand <anand21nanda@gmail.com>
2018-06-08 18:52:08 +0530

    Added js file to update quote

---
+++ app.js
@@ -0,0 +1,22 @@
+var quotesFile = chrome.runtime.getURL('data.txt');
+
+
+function showQuote() {
+
+    var quote = document.getElementById("quote");
+
+    fetch(quotesFile)
+        .then(
+            function(response) {
+                response.text().then(function(data) {
+                    data = data.split('\n');
+                    var quotesCount = data.length;
+                    var randomNum = Math.floor(Math.random() * (quotesCount - 1));
+                    quote.innerHTML = data[randomNum] + randomNum + quotesCount;
+                });
+            }
+        )
+}
+
+
+window.onload = showQuote();
